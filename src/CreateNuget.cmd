55497f1fb3fd8bf61d90fd3f9ea6c7ff923be5df
Giovanni Bassi <giggio@giggio.net>
2012-11-14 10:59:55 -0200

    publish script to nuget.org

--- src/CreateNuget.cmd
+++ src/CreateNuget.cmd
@@ -1,8 +1,18 @@
 @echo off
-echo Creating nuget file
+FOR /F %%i IN (%~dp0versionnumber.txt) DO set version=%%i
 set packagedir=%~dp0package\
 set nugetdir=%~dp0packages\NuGet.CommandLine.2.1.2\
-if not exist "%packagedir%" md "%packagedir%"
+set nugetfile=%packagedir%jasmine-headless-webkit-dotnet.nupkg
+set nugetfilecreated=%packagedir%jasmine-headless-webkit-dotnet.%version%.nupkg
+if exist "%packagedir%"*.nupkg del "%packagedir%"*.nupkg
 call "%~dp0CreateOneExec.cmd"
-"%nugetdir%tools\nuget.exe" pack "%~dp0jasmine-headless-webkit-dotnet.nuspec" -OutputDirectory %packagedir% -BasePath %~dp0 %1 %2 %3 %4
-echo Nuget package created at "%packagedir%"
+if not exist "%packagedir%" goto :error
+echo Creating nuget file
+"%nugetdir%tools\nuget.exe" pack "%~dp0jasmine-headless-webkit-dotnet.nuspec" -OutputDirectory %packagedir% -BasePath %~dp0 -Version %version% %1 %2 %3 %4
+copy "%nugetfilecreated%" "%nugetfile%"
+echo Nuget package created at "%nugetfile%"
+exit /B
+
+:error
+echo Executable not found and cannot be packaged
+exit /B 1
--- src/CreateOneExec.cmd
+++ src/CreateOneExec.cmd
@@ -1,6 +1,5 @@
 @echo off
 call "%~dp0build.cmd"
-echo Creating one executable for jasmine-headless-webkit-dotnet.exe
 set buildconfig=release
 set bindir=%~dp0jasmine-headless-webkit-dotnet\bin\%buildconfig%\
 set packagedir=%~dp0package\
@@ -8,5 +7,6 @@ set ilmergedir=%~dp0packages\ilmerge.2.12.0803\
 if not exist "%packagedir%" md "%packagedir%"
 if not "%ProgramFiles(x86)%"=="" set programfilesnormalized=%ProgramFiles(x86)%
 if "%programfilesnormalized%"=="" set programfilesnormalized=%ProgramFiles%
+echo Creating one executable for jasmine-headless-webkit-dotnet.exe
 "%ilmergedir%ilmerge.exe" /targetplatform:v4,"%programfilesnormalized%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" "%bindir%jasmine-headless-webkit-dotnet.exe" "%bindir%Args.dll" "%bindir%CoffeeSharp.dll" "%bindir%Jurassic.dll" "%bindir%Newtonsoft.Json.dll" /out:"%packagedir%\jasmine-headless-webkit-dotnet.exe" %1 %2 %3 %4
 echo Executable created at "%packagedir%jasmine-headless-webkit-dotnet.exe"
