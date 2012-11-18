51e0ccfa4a6fce75322ed3fc50dfb39293c7543d
Giovanni Bassi <giggio@giggio.net>
2012-11-17 23:17:35 -0200

    preparing new nuget file and adapting release run on x86

--- src/CreateOneExec.cmd
+++ src/CreateOneExec.cmd
@@ -8,5 +8,5 @@ if not exist "%packagedir%" md "%packagedir%"
 if not "%ProgramFiles(x86)%"=="" set programfilesnormalized=%ProgramFiles(x86)%
 if "%programfilesnormalized%"=="" set programfilesnormalized=%ProgramFiles%
 echo Creating one executable for jasmine-headless-webkit-dotnet.exe
-"%ilmergedir%ilmerge.exe" /targetplatform:v4,"%programfilesnormalized%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" "%bindir%jasmine-headless-webkit-dotnet.exe" "%bindir%Args.dll" "%bindir%CoffeeSharp.dll" "%bindir%Jurassic.dll" "%bindir%Newtonsoft.Json.dll" /out:"%packagedir%\jasmine-headless-webkit-dotnet.exe" %1 %2 %3 %4
+"%ilmergedir%ilmerge.exe" /targetplatform:v4,"%programfilesnormalized%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" "%bindir%jasmine-headless-webkit-dotnet.exe" "%bindir%Args.dll" "%bindir%CoffeeSharp.dll" "%bindir%Jurassic.dll" "%bindir%Newtonsoft.Json.dll" "%bindir%globdir.dll" /out:"%packagedir%\jasmine-headless-webkit-dotnet.exe" %1 %2 %3 %4
 echo Executable created at "%packagedir%jasmine-headless-webkit-dotnet.exe"
