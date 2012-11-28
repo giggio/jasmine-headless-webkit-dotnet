d0b590c3d618a95aab9a367821bf610bf4791339
Diullei Gomes <diullei@gmail.com>
2012-11-27 22:16:14 -0200

    Alterei a classe SourceFile para utilizar CoffeeScriptSourceCompiler

--- src/jasmine-headless-webkit-dotnet/SourceFile.cs
+++ src/jasmine-headless-webkit-dotnet/SourceFile.cs
@@ -1,42 +1,53 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CoffeeSharp;
 
 namespace jasmine_headless_webkit_dotnet
 {
     public class SourceFile
     {
+        private List<ISourceCompiler> _compilers = new List<ISourceCompiler> {new CoffeeScriptSourceCompiler()};
+
+        public bool IsAccepted()
+        {
+            return Path.EndsWith(".js", StringComparison.CurrentCultureIgnoreCase) || _compilers.Any(c => c.Accept(Path));
+        }
+
+        private string Compile()
+        {
+            if (IsAccepted())
+            {
+                return Path.EndsWith(".js", StringComparison.CurrentCultureIgnoreCase) 
+                    ? File.ReadAllText(Path) 
+                    : _compilers.FirstOrDefault(c => c.Accept(Path)).Compile(Path);
+            }
+            throw new Exception("attempt to compile an invalid file.");
+        }
+
         public SourceFile(string path)
         {
             Path = path;
         }
+
         public string CompiledPath { get; private set; }
         public string Path { get; private set; }
-        public bool IsCoffee()
-        {
-            return Path.EndsWith(".coffee", StringComparison.CurrentCultureIgnoreCase);
-        }
-
-        private static readonly CoffeeScriptEngine coffeeScriptEngine = new CoffeeScriptEngine();
 
         public virtual void Compile(string directory)
         {
             CreateCompiledPath(directory);
-            WriteCoffeeScriptContentToFile();
+            WriteCompiledContentToFile();
         }
 
-        private void WriteCoffeeScriptContentToFile()
+        private void WriteCompiledContentToFile()
         {
-            using (var reader = new StreamReader(Path))
-            {
-                var compiledCoffeeScript = coffeeScriptEngine.Compile(reader.ReadToEnd());
-                File.WriteAllText(CompiledPath, compiledCoffeeScript);
-            }
+            File.WriteAllText(CompiledPath, Compile());
         }
 
         protected void CreateCompiledPath(string directory)
         {
-            if (!IsCoffee()) throw new InvalidOperationException("Can only compile coffeescript files.");
+            if (!IsAccepted()) throw new InvalidOperationException("Can only compile coffeescript files.");
             var jsFileName = System.IO.Path.GetFileNameWithoutExtension(Path) + "-" + new Random().Next(0, 1000000) + "." + "js";
             CompiledPath = System.IO.Path.Combine(directory, jsFileName);
         }
